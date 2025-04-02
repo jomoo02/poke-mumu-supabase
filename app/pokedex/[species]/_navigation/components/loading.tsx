525d4d1dcad5963ef162d7b8293b946b7fa04aef
jomoo02 <jomootgrn88@naver.com>
2025-04-02 17:48:03 +0900

    refactor: navigation loading style

--- app/pokedex/[species]/_navigation/components/loading.tsx
+++ app/pokedex/[species]/_navigation/components/loading.tsx
@@ -12,17 +12,23 @@ export default function Loading({ species }: LoadingProps) {
   return (
     <div className="grid gap-y-3 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-0 ">
       {prevCondition && (
-        <div className="flex border-2 rounded-lg h-[52px] md:h-14 lg:h-16 border-slate-500 animate-pulse bg-gray-100">
+        <div className="flex border-2 rounded-lg h-[52px] md:h-14 lg:h-16 border-slate-500">
           <div className="flex justify-center items-center border-r-2 border-slate-500 px-3 sm:px-4">
             <ArrowLeftIcon size="1.6rem" />
+          </div>
+          <div className="flex-1 flex justify-center items-center">
+            <div className="w-1/3 bg-slate-300/50 rounded-sm h-4 animate-pulse" />
           </div>
         </div>
       )}
       <div className="lg:col-start-3 xl:col-start-3">
         {nextCondition && (
-          <div className="flex flex-row-reverse border-2 rounded-lg h-[52px] md:h-14 lg:h-16 border-slate-500 animate-pulse bg-gray-100">
+          <div className="flex flex-row-reverse border-2 rounded-lg h-[52px] md:h-14 lg:h-16 border-slate-500">
             <div className="flex justify-center items-center border-l-2 border-slate-500 px-3 sm:px-4">
               <ArrowRightIcon size="1.6rem" />
+            </div>
+            <div className="flex-1 flex justify-center items-center">
+              <div className="w-1/3 bg-slate-300/50 rounded-sm h-4 animate-pulse" />
             </div>
           </div>
         )}
