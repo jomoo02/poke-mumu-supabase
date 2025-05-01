81df2720be869f75a17e646b8715a14ebe3999da
jomoo02 <jomootgrn88@naver.com>
2025-05-01 13:49:23 +0900

    test: scroll lock(5)

--- app/hooks/useLockBodyScroll.ts
+++ app/hooks/useLockBodyScroll.ts
@@ -1,33 +1,41 @@
 import { useEffect } from 'react';
 
-function getScrollbarWidth() {
-  return window.innerWidth - document.documentElement.clientWidth;
-}
-
 export default function useLockBodyScroll(lock: boolean) {
   useEffect(() => {
     const body = document.body;
-    const originalStyle = {
-      overflow: body.style.overflow,
-      paddingRight: body.style.paddingRight,
+    const originalOverflow = body.style.overflow;
+    const originalPaddingRight = body.style.paddingRight;
+
+    const scrollbarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
+
+    // prevent touch scroll on iOS
+    const preventScroll = (e: TouchEvent) => {
+      e.preventDefault();
     };
 
     if (lock) {
-      const scrollbarWidth = getScrollbarWidth();
-
       body.style.overflow = 'hidden';
-
       if (scrollbarWidth > 0) {
         body.style.paddingRight = `${scrollbarWidth}px`;
       }
+
+      // prevent iOS touch scroll
+      document.addEventListener('touchmove', preventScroll, {
+        passive: false,
+      });
     } else {
-      body.style.overflow = originalStyle.overflow;
-      body.style.paddingRight = originalStyle.paddingRight;
+      body.style.overflow = originalOverflow;
+      body.style.paddingRight = originalPaddingRight;
+
+      document.removeEventListener('touchmove', preventScroll);
     }
 
     return () => {
-      body.style.overflow = originalStyle.overflow;
-      body.style.paddingRight = originalStyle.paddingRight;
+      body.style.overflow = originalOverflow;
+      body.style.paddingRight = originalPaddingRight;
+
+      document.removeEventListener('touchmove', preventScroll);
     };
   }, [lock]);
 }
