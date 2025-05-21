e078e2c35791d02c9a60602dcca8f7c073abe747
jomoo02 <jomootgrn88@naver.com>
2025-05-21 16:30:50 +0900

    test: nav-button font-size(2)

--- app/features/species/components/species-nav-button.tsx
+++ app/features/species/components/species-nav-button.tsx
@@ -14,7 +14,7 @@ function Prev({ poke }: NavButtonProps) {
   return (
     <Link
       href={`/pokedex/${no}/${species}`}
-      className="flex rounded-lg bg-transparent hover:bg-neutral-300/40 transition-colors duration-200 py-2 sm:px-2"
+      className="flex rounded-lg bg-transparent hover:bg-neutral-300/40 active:bg-neutral-300/40 transition-colors duration-200 py-2 sm:px-2"
       prefetch
     >
       <div className="flex justify-center items-center pr-1 sm:pr-2">
@@ -34,13 +34,13 @@ function Next({ poke }: NavButtonProps) {
   return (
     <Link
       href={`/pokedex/${no}/${species}`}
-      className="flex flex-row-reverse rounded-lg hover:bg-neutral-300/40 transition-colors duration-200 py-2 sm:px-2"
+      className="flex flex-row-reverse rounded-lg hover:bg-neutral-300/40 active:bg-neutral-300/40 transition-colors duration-200 py-2 sm:px-2"
       prefetch
     >
       <div className="flex justify-center items-center pl-1 sm:pl-2">
         <ArrowRightIcon size="1.2rem" />
       </div>
-      <div className="flex h-full gap-x-1.5 justify-center items-center font-medium text-[15px] text-slate-800">
+      <div className="flex h-full gap-x-1.5 justify-center items-center font-semibold text-base text-slate-800">
         <span>{`${formatPokedexNumber(no)}`}</span>
         <span>{name}</span>
       </div>
