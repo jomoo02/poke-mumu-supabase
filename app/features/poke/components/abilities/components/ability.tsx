7c40c8606f59530906a9a8eb2d6c4b12e3535bb5
jomoo02 <jomootgrn88@naver.com>
2025-05-22 22:11:27 +0900

    test: antialiased 제거 테스트

--- app/features/poke/components/abilities/components/ability.tsx
+++ app/features/poke/components/abilities/components/ability.tsx
@@ -12,13 +12,11 @@ export default function Ability({ isHidden, name, flavorText }: AbilityProps) {
       }`}
     >
       <div className="w-[6.75rem] sm:w-[10.5rem] border-r border-slate-300 py-3 px-2 md:px-3 flex flex-col justify-center items-center text-center">
-        <div className="truncate text-zinc-900 font-medium w-full">{name}</div>
-        {isHidden && (
-          <div className="text-sm font-medium text-zinc-800">(숨겨진 특성)</div>
-        )}
+        <div className="truncate text-zinc-950 w-full">{name}</div>
+        {isHidden && <div className="text-sm text-zinc-800">(숨겨진 특성)</div>}
       </div>
 
-      <div className="flex-1 p-3 flex items-center">
+      <div className="flex-1 p-3 sm:p-4 flex items-center">
         <p className="text-pretty text-zinc-950">{flavorText}</p>
       </div>
     </div>
--- app/features/poke/components/pokedex-information/components/info.tsx
+++ app/features/poke/components/pokedex-information/components/info.tsx
@@ -9,7 +9,7 @@ export default function Info({ subject, children }: InfoProps) {
       <div className="min-w-[4.5rem] w-[4.5rem] text-right pr-2 font-medium text-zinc-500">
         {subject}
       </div>
-      <div className="pl-3 text-zinc-800 font-medium">{children}</div>
+      <div className="pl-3 text-zinc-950">{children}</div>
     </div>
   );
 }
