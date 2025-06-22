8fb749a17c2c7489547aa2ceb77950f83b434c86
jomoo02 <jomootgrn88@naver.com>
2025-06-22 21:37:28 +0900

    test: 모바일 모달(9)

--- app/features/search/hooks/useLockScroll.ts
+++ app/features/search/hooks/useLockScroll.ts
@@ -1,24 +1,52 @@
+// import { useEffect } from 'react';
+
+// export default function useLockBodyScroll(lock: boolean) {
+//   useEffect(() => {
+//     const body = document.body;
+//     const originalOverflow = body.style.overflow;
+//     const originalPaddingRight = body.style.paddingRight;
+
+//     const scrollbarWidth =
+//       window.innerWidth - document.documentElement.clientWidth;
+
+//     if (lock) {
+//       body.style.overflow = 'hidden';
+//       if (scrollbarWidth > 0) {
+//         body.style.paddingRight = `${scrollbarWidth}px`;
+//       }
+//     }
+
+//     return () => {
+//       body.style.overflow = originalOverflow;
+//       body.style.paddingRight = originalPaddingRight;
+//     };
+//   }, [lock]);
+// }
 import { useEffect } from 'react';
 
 export default function useLockBodyScroll(lock: boolean) {
   useEffect(() => {
+    const scrollY = window.scrollY;
     const body = document.body;
-    const originalOverflow = body.style.overflow;
-    const originalPaddingRight = body.style.paddingRight;
-
-    const scrollbarWidth =
-      window.innerWidth - document.documentElement.clientWidth;
 
     if (lock) {
+      body.style.position = 'fixed';
+      body.style.top = `-${scrollY}px`;
+      body.style.width = '100%';
       body.style.overflow = 'hidden';
-      if (scrollbarWidth > 0) {
-        body.style.paddingRight = `${scrollbarWidth}px`;
-      }
     }
 
     return () => {
-      body.style.overflow = originalOverflow;
-      body.style.paddingRight = originalPaddingRight;
+      const top = body.style.top;
+      body.style.position = '';
+      body.style.top = '';
+      body.style.overflow = '';
+      body.style.width = '';
+
+      if (top) {
+        const scrollToY = parseInt(top) * -1;
+        window.scrollTo(0, scrollToY);
+      }
     };
   }, [lock]);
 }
