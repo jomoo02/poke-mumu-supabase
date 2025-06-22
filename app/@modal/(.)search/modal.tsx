283d60a2091f5228fcbe934d2e7c9215039e4499
jomoo02 <jomootgrn88@naver.com>
2025-06-22 19:27:46 +0900

    test: 모바일 모달(1)

--- app/@modal/(.)search/modal.tsx
+++ app/@modal/(.)search/modal.tsx
@@ -5,8 +5,8 @@ import { createPortal } from 'react-dom';
 
 export default function Modal() {
   return createPortal(
-    <div className="backdrop-blur-sm bg-zinc-900/30 z-[5000] top-0 left-0 fixed w-screen h-screen flex justify-center lg:items-center">
-      <div className="w-full h-full lg:w-[650px] lg:h-[500px]">
+    <div className="backdrop-blur-sm bg-zinc-900/30 z-[5000] top-0 bottom-0 left-0 fixed w-screen h-screen flex justify-center lg:items-center">
+      <div className="w-full h-full min-h-full max-h-full lg:w-[650px] lg:h-[500px] lg:min-h-[500px]">
         <Search />
       </div>
     </div>,
