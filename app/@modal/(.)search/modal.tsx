895af13d3b4e7224a2ea56ec89b3c53910ea08c7
jomoo02 <jomootgrn88@naver.com>
2025-06-24 15:44:34 +0900

    test: 모바일 모달(20)

--- app/@modal/(.)search/modal.tsx
+++ app/@modal/(.)search/modal.tsx
@@ -4,8 +4,8 @@ import Search from '@/app/features/search/containers/search';
 
 export default function Modal() {
   return (
-    <div className="lg:backdrop-blur-sm bg-white lg:bg-slate-900/30 z-[5000] inset-0 fixed w-screen h-full flex justify-center lg:items-center">
-      <div className="w-full h-full lg:w-[650px] lg:h-[500px]">
+    <div className="lg:backdrop-blur-sm bg-white lg:bg-slate-900/30 z-[5000] inset-0 fixed w-screen h-dvh flex justify-center lg:items-center">
+      <div className="w-full h-dvh lg:w-[650px] lg:h-[500px]">
         <Search />
       </div>
     </div>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,8 +2,8 @@ import Search from '../features/search/containers/search';
 
 export default function Page() {
   return (
-    <div className="lg:backdrop-blur-sm bg-white lg:bg-zinc-900/30 z-[5000] top-0 left-0 fixed w-screen h-full flex justify-center lg:items-center">
-      <div className="w-full h-full lg:w-[650px] lg:h-[500px]">
+    <div className="lg:backdrop-blur-sm bg-white lg:bg-zinc-900/30 z-[5000] top-0 left-0 fixed w-screen h-dvh flex justify-center lg:items-center">
+      <div className="w-full h-dvh lg:w-[650px] lg:h-[500px]">
         <Search />
       </div>
     </div>
