c87e7070557c3e91a5246a8587526a1f9ddea80a
jomoo02 <jomootgrn88@naver.com>
2025-06-24 14:24:31 +0900

    test: 모바일 모달(17)

--- app/@modal/(.)search/modal.tsx
+++ app/@modal/(.)search/modal.tsx
@@ -4,8 +4,8 @@ import Search from '@/app/features/search/containers/search';
 
 export default function Modal() {
   return (
-    <div className="backdrop-blur-sm bg-zinc-900/30 z-[5000] inset-0 fixed w-screen h-full flex justify-center lg:items-center">
-      <div className="w-full max-h-svh lg:w-[650px] lg:h-[500px]">
+    <div className="lg:backdrop-blur-sm bg-white lg:bg-zinc-900/30 z-[5000] inset-0 fixed w-screen h-full flex justify-center lg:items-center">
+      <div className="w-full max-h-lvh lg:w-[650px] lg:h-[500px]">
         <Search />
       </div>
     </div>
