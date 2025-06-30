95ce9ff935567d4dc13b817190a2291a121e4e6f
jomoo02 <jomootgrn88@naver.com>
2025-06-30 20:48:57 +0900

    test: combobox scroll 시 open 상태 유지(4)

--- app/components/combobox/combobox-input/ussComboboxInput.ts
+++ app/components/combobox/combobox-input/ussComboboxInput.ts
@@ -1,21 +1,22 @@
-import { useRef, useEffect } from 'react';
-import { useInputValue, useListOpen } from '../combobox.context';
+import { useRef } from 'react';
+import { useInputValue } from '../combobox.context';
 import { useHandleKeyDown } from './useListHandle';
 
 export default function useComboboxInput() {
   const inputRef = useRef<HTMLInputElement>(null);
   const { inputValue, onChangeInputValue } = useInputValue();
-  const { isOpen } = useListOpen();
+  // const { isOpen } = useListOpen();
   const { handleKeyDown } = useHandleKeyDown();
-  useEffect(() => {
-    if (isOpen) {
-      requestAnimationFrame(() => {
-        requestAnimationFrame(() => {
-          inputRef.current?.focus({ preventScroll: true });
-        });
-      });
-    }
-  }, [isOpen]);
+
+  // useEffect(() => {
+  //   if (isOpen) {
+  //     requestAnimationFrame(() => {
+  //       requestAnimationFrame(() => {
+  //         inputRef.current?.focus({ preventScroll: true });
+  //       });
+  //     });
+  //   }
+  // }, [isOpen]);
 
   return {
     inputRef,
