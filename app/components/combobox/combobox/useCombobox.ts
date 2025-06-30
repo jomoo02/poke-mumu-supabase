d8de8297aa3baa4f47e19b0e40623a7e8d6e1e85
jomoo02 <jomootgrn88@naver.com>
2025-06-30 20:18:30 +0900

    test: combobox scroll 시 open 상태 유지(2)

--- app/components/combobox/combobox/useCombobox.ts
+++ app/components/combobox/combobox/useCombobox.ts
@@ -46,6 +46,9 @@ export default function useCombobox(
 
   const close = () => {
     setIsOpen(false);
+    // setTimeout(() => {
+    //   triggerRef.current?.focus({ preventScroll: true });
+    // }, 0);
     requestAnimationFrame(() => {
       triggerRef.current?.focus({ preventScroll: true });
     });
