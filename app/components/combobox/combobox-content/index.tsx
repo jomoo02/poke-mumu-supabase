17bb1391dcd3d0bb88fe0ca9324817a27378bc77
jomoo02 <jomootgrn88@naver.com>
2025-07-02 16:17:47 +0900

    test: mobile combobox input click(4)

--- app/components/combobox/combobox-content/index.tsx
+++ app/components/combobox/combobox-content/index.tsx
@@ -26,8 +26,6 @@ export default function ComboboxContent({ children }: ComboboxContentProps) {
     <div
       ref={contentRef}
       style={hasPosition ? position : hiddenStyle}
-      // tabIndex={-1}
-      // onKeyDown={handleKeyDown}
       className={`bg-white shadow-xl p-1 w-full border border-gray-300 rounded-md outline-none focus:outline-none  h-52 max-h-52 overflow-hidden
         transition-opacity duration-200 ease-out transform z-50
         scale-95 opacity-0 ${show && 'scale-100 opacity-100'}
--- app/components/combobox/combobox-input/ussComboboxInput.ts
+++ app/components/combobox/combobox-input/ussComboboxInput.ts
@@ -1,31 +1,31 @@
-import { useRef, useEffect } from 'react';
+import { useRef } from 'react';
 import {
   useInputValue,
-  useListOpen,
-  useHasPosition,
+  // useListOpen,
+  // useHasPosition,
 } from '../combobox.context';
 import { useHandleKeyDown } from './useListHandle';
 
 export default function useComboboxInput() {
   const inputRef = useRef<HTMLInputElement>(null);
   const { inputValue, onChangeInputValue } = useInputValue();
-  const { isOpen } = useListOpen();
+  // const { isOpen } = useListOpen();
   const { handleKeyDown } = useHandleKeyDown();
-  const { hasPosition } = useHasPosition();
+  // const { hasPosition } = useHasPosition();
 
-  useEffect(() => {
-    if (isOpen && hasPosition) {
-      const id1 = requestAnimationFrame(() => {
-        requestAnimationFrame(() => {
-          inputRef.current?.focus({ preventScroll: true });
-        });
-      });
+  // useEffect(() => {
+  //   if (isOpen && hasPosition) {
+  //     const id1 = requestAnimationFrame(() => {
+  //       requestAnimationFrame(() => {
+  //         inputRef.current?.focus({ preventScroll: true });
+  //       });
+  //     });
 
-      return () => {
-        cancelAnimationFrame(id1);
-      };
-    }
-  }, [isOpen, hasPosition]);
+  //     return () => {
+  //       cancelAnimationFrame(id1);
+  //     };
+  //   }
+  // }, [isOpen, hasPosition]);
 
   return {
     inputRef,
